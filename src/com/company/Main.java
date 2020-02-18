5ccd9f773783ac91944e0966ac4189b634234881
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-02-19 01:44:40 +0300

    Read all rows in file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,9 @@ import java.util.Arrays;
 public class Main
 {
     public static void main(String[] args) throws IOException {
-        CSVReader reader = new CSVReader(new java.io.FileReader("LabWork1.csv"), ',', '"', 1);
+        CSVReader reader = new CSVReader(new java.io.FileReader("LabWork1.csv"), ',', '"', 0);
         String[] nextLine;
-        while ((nextLine = reader.readNext()) != null)
+        while ((nextLine =  reader.readNext()) != null)
         {
             if (nextLine != null)
             {
