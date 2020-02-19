0b3dcee2c7fa0dd39caca8b6ff11b0cce6ee3989
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-02-19 14:00:56 +0300

    Updated class Table

--- src/LabWork1/Table.java
+++ src/LabWork1/Table.java
@@ -1,4 +1,17 @@
 package LabWork1;
 
-public class Table {
+public class Table extends Furniture
+{
+    public Table(String type, String name, int width, int height, int length, String color)
+    {
+        super(type, name, width, height, length, color);
+    }
+
+    public void LookInfo()
+    {
+        System.out.println("Main Info:");
+        System.out.printf("Type: %s\nName %s\nWidth: %d\nHeight %d\nLength: %d\nColor %s\n",
+                type, name, width, height, length, color);
+        System.out.print("\n");
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import LabWork1.Chair;
 import LabWork1.Reader;
+import LabWork1.Table;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,6 +15,8 @@ public class Main
        Reader.PrintData(result);
        Chair test = new Chair("Chair", "Test", 50,60,70,"Red");
        test.LookInfo();
+       Table table = new Table("Table", "Test1", 50,60,70,"Red");
+       table.LookInfo();
 
        /*
         ObjectOutputStream oos = new ObjectOutputStream(
