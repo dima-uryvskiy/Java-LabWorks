8a829f10e0eb152eaf9b36c5f9c399d82d72f850
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-03-04 22:54:57 +0300

    Create table  and class Furniture.java

---
+++ LabWork2/src/sample/Furniture.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.beans.property.*;
+
+public class Furniture {
+
+    private SimpleStringProperty type;
+    private SimpleStringProperty name;
+    private SimpleIntegerProperty width;
+    private SimpleIntegerProperty height;
+    private SimpleIntegerProperty length;
+    private SimpleStringProperty color;
+
+    public Furniture(String type, String name, int width, int height, int length, String color)
+    {
+        this.type = new SimpleStringProperty(type);
+        this.name = new SimpleStringProperty(name);
+        this.width = new SimpleIntegerProperty(width);
+        this.height = new SimpleIntegerProperty(height);
+        this.length = new SimpleIntegerProperty(length);
+        this.color = new SimpleStringProperty(color);
+    }
+
+    public String getType(){ return type.get();}
+    public void setType(String value){ type.set(value);}
+
+    public String getName(){ return name.get();}
+    public void setName(String value){ name.set(value);}
+
+    public int getWidth(){ return width.get();}
+    public void setWidth(int value){width.set(value);}
+
+    public int getHeight(){ return height.get();}
+    public void setHeight(int value){ height.set(value);}
+
+    public int getLength(){ return length.get();}
+    public void setLength(int value){ length.set(value);}
+
+    public String getColor(){ return color.get();}
+    public void setColor(String value){ color.set(value);}
+
+    public void LookInfo()
+    {
+        System.out.println("Main Info:");
+        System.out.printf("Type: %s\nName: %s\nWidth: %d\nHeight: %d\nLength: %d\nColor: %s\n",
+                type, name, width, height, length, color);
+        System.out.print("\n");
+    }
+}
--- LabWork2/src/sample/Main.java
+++ LabWork2/src/sample/Main.java
@@ -1,23 +1,73 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 670, 400));
-        primaryStage.show();
-
-    }
+public class Main extends Application{
 
     public static void main(String[] args) {
-        launch(args);
+
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // создаем список объектов
+        ObservableList<Furniture> furniture = FXCollections.observableArrayList(
+                new Furniture("table","Maxsi", 70,45,66, "black"),
+                new Furniture("chair","Kristofer", 90,90,90, "red"),
+                new Furniture("table","MacBook", 67,48,77, "green"),
+                new Furniture("chair","Kybay", 99,33,66, "yellow")
+        );
+
+        // определяем таблицу и устанавливаем данные
+        TableView<Furniture> table = new TableView<Furniture>(furniture);
+        table.setPrefWidth(500);
+        table.setPrefHeight(300);
+
+        // столбец для вывода имени
+        TableColumn<Furniture, String> typeColumn = new TableColumn<Furniture, String>("Type");
+        // определяем фабрику для столбца с привязкой к свойству name
+        typeColumn.setCellValueFactory(new PropertyValueFactory<Furniture, String>("type"));
+        // добавляем столбец
+        table.getColumns().add(typeColumn);
+
+        TableColumn<Furniture, String> nameColumn = new TableColumn<Furniture, String>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Furniture, String>("name"));
+        table.getColumns().add(nameColumn);
+
+        // столбец для вывода возраста
+        TableColumn<Furniture, Integer> widthColumn = new TableColumn<Furniture, Integer>("Width");
+        widthColumn.setCellValueFactory(new PropertyValueFactory<Furniture, Integer>("width"));
+        table.getColumns().add(widthColumn);
+
+        TableColumn<Furniture, Integer> heightColumn = new TableColumn<Furniture, Integer>("Height");
+        heightColumn.setCellValueFactory(new PropertyValueFactory<Furniture, Integer>("height"));
+        table.getColumns().add(heightColumn);
+
+        TableColumn<Furniture, Integer> lengthColumn = new TableColumn<Furniture, Integer>("Length");
+        lengthColumn.setCellValueFactory(new PropertyValueFactory<Furniture, Integer>("length"));
+        table.getColumns().add(lengthColumn);
+
+        TableColumn<Furniture, String> colorColumn = new TableColumn<Furniture, String>("Color");
+        colorColumn.setCellValueFactory(new PropertyValueFactory<Furniture, String>("color"));
+        table.getColumns().add(colorColumn);
+
+
+        FlowPane root = new FlowPane(10, 10, table);
+
+        Scene scene = new Scene(root, 670, 400);
+
+        stage.setScene(scene);
+        stage.setTitle("TableView in JavaFX");
+        stage.show();
     }
 }
