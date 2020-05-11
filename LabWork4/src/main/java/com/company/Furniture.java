7413947050a0d431e308ca86510390ce2ec9a230
dima-uryvskiy <dima-uryvskiyy@rambler.ru>
2020-05-12 00:48:48 +0300

    Add Hibernate class and annotation Furniture

--- LabWork4/src/main/java/com/company/Furniture.java
+++ LabWork4/src/main/java/com/company/Furniture.java
@@ -1,28 +1,80 @@
 package com.company;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "furnitures")
 public class Furniture
 {
+    private Long id;
+    private String type;
+    private String name;
+    private int width;
+    private int height;
+    private int length;
+    private String color;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    public String type;
-    public String name;
-    public int width;
-    public int height;
-    public int length;
-    public String color;
+    public Long getId() {
+        return id;
+    }
 
-    public Furniture(String []  parameters)
-    {
-        this.type = parameters[0];
-        this.name = parameters[1];
-        this.width = Integer.valueOf(parameters[2]);
-        this.height = Integer.valueOf(parameters[3]);
-        this.length = Integer.valueOf(parameters[4]);
-        this.color = parameters[5];
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column (name = "type")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Column (name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column (name = "width")
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Column (name = "height")
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Column (name = "length")
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Column (name = "color")
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public void LookInfo()
--- LabWork4/src/main/java/com/company/Main.java
+++ LabWork4/src/main/java/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import com.company.util.HibernateUtil;
+import org.hibernate.classic.Session;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -13,12 +16,28 @@ public class Main
 
         for (String[] furnitureData: data)
         {
-                furnitures.add(new Furniture(furnitureData));
+            Furniture furniture = new Furniture();
+            furniture.setType(furnitureData[0]);
+            furniture.setName(furnitureData[1]);
+            furniture.setWidth(Integer.valueOf(furnitureData[2]));
+            furniture.setHeight(Integer.valueOf(furnitureData[3]));
+            furniture.setLength(Integer.valueOf(furnitureData[4]));
+            furniture.setColor(furnitureData[5]);
+
+            furnitures.add(furniture);
         }
 
         for (Furniture obj: furnitures)
             obj.LookInfo();
 
 
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        session.save(furnitures.get(3));
+
+        session.getTransaction().commit();
     }
 }
